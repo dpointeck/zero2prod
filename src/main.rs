afc9cca828c8eb04a989db775539d999bb5687fb
Daniel Pointecker <daniel.pointecker@gmail.com>
2022-11-07 08:07:47 +0100

    extend ci

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
 use actix_web::middleware::Logger;
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
@@ -12,16 +12,14 @@ async fn health_check(_req: HttpRequest) -> impl Responder {
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new( || {
+    HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(greet))
             .route("/health_check", web::get().to(health_check))
             .route("/{name}", web::get().to(greet))
-
             .wrap(Logger::default())
     })
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
-
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 }
