517944ef640803a1de23ab9650d0c3359fe35586
dpointeck <daniel.pointecker@gmail.com>
2023-01-03 11:04:30 +0100

    🫥

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,20 @@
+pub mod configuration;
+pub mod routes;
+pub mod startup;
+
 use actix_web::{web, App, HttpServer, HttpResponse};
 use actix_web::dev::Server;
 use std::net::TcpListener;
+use crate::routes::{health_check, subscribe};
 
-async fn health_check() -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
 
 #[derive(serde::Deserialize)]
-struct FormData {
+pub struct FormData {
     email: String,
     name: String
 }
 
-async fn subscribe(_form: web::Form<FormData>) -> HttpResponse { HttpResponse::Ok().finish() }
+
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
